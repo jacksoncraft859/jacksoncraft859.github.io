4625081699524bc426b6306a76231f3afb7dd6c9
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-13 15:59:29 -0400

    Update inspectelementstuff.js

--- inspectelementstuff.js
+++ inspectelementstuff.js
@@ -139,27 +139,23 @@ function hide_panel(){
 };
 
 panel = create_tag("div")
-
 panelbar = create_tag("div")
-
 //panelbar.innerHTML = "<a href='#' onclick='hide_panel()' id='panel_switch' >Hide Panel<a>"
 panelbar.innerHTML = "JS PANEL" ;
 // panelbar.style.position = "fixed"
-panelbar.style"height:20px;background-color:orange;z-index:10e5";
-panelbar.onclick = hide_panel;
-
+panelbar.style.height = "20px";
+panelbar.style["background-color"] = "orange";
+panelbar.style["z-index"] = 10e5;
+panelbar.onclick = hide_panel ;
 subpanel = create_tag("div")
-
-
 panel.id = "InjectorPanel";
+panel.style.position = "fixed";
+panel.style.width = "150px";
 //panel.style.height = "500px";
-panel.style="color:white;background-color:black;width:150px;position:fixed;top:0px;";
-
-
-
+panel.style.top = "0px";
+panel.style="color:white;background-color:black;";
 panel.appendChild(panelbar);
 panel.appendChild(subpanel);
-
 document.querySelector("body").appendChild(panel);
 
  
