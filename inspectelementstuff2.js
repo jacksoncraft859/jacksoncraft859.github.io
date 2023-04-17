16268f1e3a901d53cb46a8cd82987d4d83fe5e41
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-17 13:07:19 -0400

    Update inspectelementstuff2.js

--- inspectelementstuff2.js
+++ inspectelementstuff2.js
@@ -231,6 +231,7 @@ panel.style["color"] = "white";
 panel.appendChild(panelbar);
 panel.appendChild(subpanel);
 panel.appendChild(subpan2);
+panel.appendChild(subpan3);
 panel.onload = hide_subpanel ;
 
 document.querySelector("body").appendChild(panel);
