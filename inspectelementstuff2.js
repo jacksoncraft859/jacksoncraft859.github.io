b7482d988a1294072e5f7afc02509aef060d2ed0
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-18 12:24:34 -0400

    Update inspectelementstuff2.js

--- inspectelementstuff2.js
+++ inspectelementstuff2.js
@@ -243,10 +243,12 @@ panelbar = create_tag("div")
 panelbar.innerHTML = "JS PANEL" ;
 
 // panelbar.style.position = "fixed"
+panelbar.id = "InjectorPanel bar";
 panelbar.style.height = "20px";
 panelbar.style["background-color"] = "orange";
 panelbar.style["z-index"] = 10e5;
 panelbar.onclick = hide_panel ;
+panelbar.style.cursor = "move";
 
 subpanel = create_tag("div")
 
@@ -260,7 +262,7 @@ subpan4 = create_tag("div")
 subpan4.id = "subpanels";
 
 panel.id = "InjectorPanel";
-panel.style.position = "fixed";
+panel.style.position = "absolute";
 panel.style.right = "150px";
 panel.style.width = "250px";
 //panel.style.height = "500px";
@@ -280,6 +282,49 @@ document.querySelector("body").appendChild(panel);
  
 // Serch Web Archive
 //htmlcode =  "javascript:(function(){window.location='http://web.archive.org/web/*/' + document.URL})()' > Web Archive</a></ul>" ;
+
+dragElement(document.getElementById("InjectorPanel"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "bar")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "bar").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 //==================
 add_label("Inspect Element");
