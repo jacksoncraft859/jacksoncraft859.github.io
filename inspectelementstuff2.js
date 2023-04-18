1113e1247704dbe0fbe08af8c8504b09916693c7
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-18 09:31:24 -0400

    Update inspectelementstuff2.js

--- inspectelementstuff2.js
+++ inspectelementstuff2.js
@@ -325,7 +325,7 @@ add_mark("Remove Cookies", "javascript:void(document.cookie=null)");
 add_mark("PwnYoutube", "javascript:(function(){url='http://deturl.com/download-video.js';document.body.appendChild(document.createElement('script')).src=url+'?'+new Date().getTime();})();");
 add_mark("Full URLs", fullUrls)
 add_label("Proxy & Cloaking");
-add_mark("Interstellar", "https://gointerstellar.app/");
+add_mark("Interstellar", "https://goarmy.us.to/");
 add_mark("Tab Cloaking", "javascript:document.title=prompt('Welcome to the Tab Cloak setup!\n\nEnter the title you want to set for this tab::');var icon=document.querySelector(`link[rel='icon']`);if (!icon) {icon = document.createElement('link');icon.rel='icon';};switch(prompt('What icon would you like to use?\n\n[1] Google Search\n[2] Google Drive\n[3] Custom URL\n\nPlease only enter a number!%27)){case%271%27:icon.setAttribute(%27href%27,%27https://www.google.com/favicon.ico%27);break;case%272%27:icon.setAttribute(%27href%27,%27https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png%27);break;case%273%27:icon.setAttribute(%27href%27,prompt(%27Please enter the URL for the icon you want:%27));} document.head.appendChild(icon);");
 add_mark("Close", remove_panel);
 
@@ -333,3 +333,9 @@ subpanel.appendChild(UL);
 subpan2.appendChild(UL2);
 subpan3.appendChild(UL3);
 subpan4.appendChild(UL4);
+
+panel.onload = function() {
+  hide_subpanel ;
+  hide_subpanel2 ;
+  hide_subpanel3 ;
+};
