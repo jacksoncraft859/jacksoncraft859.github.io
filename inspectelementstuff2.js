7cdf828d79f4db5d574013d10ec93b8aaa7af11c
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-18 12:50:43 -0400

    Update inspectelementstuff2.js

--- inspectelementstuff2.js
+++ inspectelementstuff2.js
@@ -386,9 +386,6 @@ subpanel.appendChild(UL);
 subpan2.appendChild(UL2);
 subpan3.appendChild(UL3);
 subpan4.appendChild(UL4);
-
-document.getElementById("subpanels").onload = function() {
-  hide_subpanel ;
-  hide_subpanel2 ;
-  hide_subpanel3 ;
-};
+hide_subpanel ;
+hide_subpanel2 ;
+hide_subpanel3 ;
