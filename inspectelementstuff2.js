b99a624672d79e587220b642bfa70b28c0cdb66e
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-18 13:47:28 -0400

    Update inspectelementstuff2.js

--- inspectelementstuff2.js
+++ inspectelementstuff2.js
@@ -372,6 +372,7 @@ add_submark2("Run Functional.js",
 add_submark2("Execute HTML", "javascript:var txt='';function getSelText(wndw){var sel='';if(document.all){sel=wndw.document.selection.createRange().text;}else{sel=wndw.window.getSelection();}return sel;}void(frms=window.frames);if(frms.length==0){txt=getSelText(window);}else{for(iQA=0;iQA&lt;frms.length;iQA++){void(txt=getSelText(frms[iQA]));if(txt.length&gt;0){break;}}}while(txt.length==0){txt=promt('Input:');}win=window.open('','','');void(win.document.write(txt));void(win.document.close())");
 add_submark2("Show Cookies", ShowCookies);
 add_label("Multitools")
+add_mark("ClockWork", "javascript:var s = document.createElement('script');s.type='text/javascript';s.src='https://redstone-nw.netlify.app/assets/clockwork-bookmarklet.js';document.body.appendChild(s);");
 add_mark("Multitool 1", "javascript:(function () %7Bvar v %3D document.createElement(%27script%27)%3Bv.src %3D %27https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FBrowncha023%2FVengeance%40v1.2.0%2Fscript.min.js%27%3Bdocument.body.appendChild(v)%3B%7D())");
 //==================
 add_label("Security and Auditing");
