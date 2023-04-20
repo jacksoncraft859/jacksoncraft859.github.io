8894cb353a0a16bee8e551a79e8fa7b4d9e38cd1
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-20 12:43:09 -0400

    Update inspectelementstuff3.js

--- inspectelementstuff3.js
+++ inspectelementstuff3.js
@@ -282,7 +282,7 @@ var head = document.getElementsByTagName('HEAD')[0];
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = 'default_theme.css';
+link.href = 'jacksoncraft859.github.io/default_theme.css';
 head.appendChild(link);
 
 // Serch Web Archive
