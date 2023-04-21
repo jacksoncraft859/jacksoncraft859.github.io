19e002056a2248831d44c91a5510cc2bb9e05b33
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-21 10:47:44 -0400

    fix?

--- inspectelementstuff3-1.js
+++ inspectelementstuff3-1.js
@@ -243,7 +243,7 @@ panelbar.innerHTML = "JS PANEL" ;
 
 // panelbar.style.position = "fixed"
 panelbar.id = "InjectorPanel bar";
-panelbar.className = "panelbar"
+panelbar.className = "panelbar" ;
 panelbar.style.height = "20px";
 panelbar.style["z-index"] = 10e5;
 panelbar.onclick = hide_panel ;
