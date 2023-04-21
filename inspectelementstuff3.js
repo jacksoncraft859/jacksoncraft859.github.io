0b13f2d63ddd62bac3cc5c0d386658febf285853
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-21 10:32:36 -0400

    Update inspectelementstuff3.js

--- inspectelementstuff3.js
+++ inspectelementstuff3.js
@@ -255,15 +255,12 @@ subpanel.id = "subpanels";
 
 subpan2 = create_tag("div");
 subpan2.className = "subpanels";
-subpan2.id = "subpanels";
 
 subpan3 = create_tag("div");
 subpan3.className = "subpanels";
-subpan3.id = "subpanels";
 
 subpan4 = create_tag("div");
 subpan4.className = "subpanels";
-subpan4.id = "subpanels";
 
 panel.id = "InjectorPanel";
 panel.className = "panel";
