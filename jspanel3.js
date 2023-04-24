100ccd80313aa155ea3d44c125acf343c6ce3bda
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-24 12:24:23 -0400

    Update jspanel3.js

--- jspanel3.js
+++ jspanel3.js
@@ -386,10 +386,6 @@ function dragElement(elmnt) {
 add_label("Tests")
 add_mark("Blue Theme", change_theme_blue);
 add_mark("Default Theme", change_theme_orange);
-add_mark("Green Theme", change_theme_green);
-add_mark("Red Theme", change_theme_red);
-add_mark("Pink Theme", change_theme_pink);
-add_mark("Yellow Theme,"change_theme_yellow);
 add_label("Inspect Element");
 add_mark("Show Inspect panel", hide_subpanel);
 add_sublabel("Inspect");
