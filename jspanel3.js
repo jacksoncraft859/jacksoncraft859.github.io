5b769cebbdbf9978ee07b19159cd5b8996b7fd14
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-28 11:40:39 -0400

    omg i'm about to lose my mind

--- jspanel3.js
+++ jspanel3.js
@@ -310,7 +310,7 @@ function change_theme_hack(){
 function launchvm(){
     vm = create_tag("script");
     vm.type = "text/javascript";
-    vm.src = 'https://jacksoncraft859.github.io/versionmanager.js';
+    vm.setAttribute("src", "https://jacksoncraft859.github.io/versionmanager.js");
     document.querySelector("body").appendChild(vm);
     remove_panel();
 };
