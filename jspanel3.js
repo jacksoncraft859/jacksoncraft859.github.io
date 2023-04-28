5fabd3142559c6e52f7e7fbf171b2e2b673ec599
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-04-28 12:16:35 -0400

    made changes

--- jspanel3.js
+++ jspanel3.js
@@ -295,7 +295,9 @@ function change_theme_hack(){
 };
 
 function launchvm(){
-    vm.innerHTML = "<script src='https://jacksoncraft859.github.io/versionmanager.js></script>"
+    vm = create_tag("script");
+    vm.type = "text/javascript";
+    vm.setAttribute("src", "https://jacksoncraft859.github.io/versionmanager.js");
     document.querySelector("body").appendChild(vm);
     remove_panel();
 };
@@ -454,7 +456,7 @@ add_mark("Multitool 1", "javascript:(function () %7Bvar v %3D document.createEle
 add_label("Proxy & Cloaking");
 add_mark("Interstellar", "https://goarmy.us.to/");
 add_mark("Tab Cloaking", "javascript:document.title=prompt('Welcome to the Tab Cloak setup!\n\nEnter the title you want to set for this tab::');var icon=document.querySelector(`link[rel='icon']`);if (!icon) {icon = document.createElement('link');icon.rel='icon';};switch(prompt('What icon would you like to use?\n\n[1] Google Search\n[2] Google Drive\n[3] Custom URL\n\nPlease only enter a number!%27)){case%271%27:icon.setAttribute(%27href%27,%27https://www.google.com/favicon.ico%27);break;case%272%27:icon.setAttribute(%27href%27,%27https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png%27);break;case%273%27:icon.setAttribute(%27href%27,prompt(%27Please enter the URL for the icon you want:%27));} document.head.appendChild(icon);");
-add_mark("Version Manager", launchvm);
+add_mark("Version Manager", "javascript:var vm = document.createElement('script');vm.type='text/javascript';vm.src='https://jacksoncraft859.github.io/versionmanager.js';document.body.appendChild(vm);", remove_panel);
 add_mark("Close", remove_panel);
 
 subpanel.appendChild(UL);
