ea8417f8248962e5f48c5d539ce10ba04d10cea8
Jacksoncraft859 <pollardjackson@outlook.com>
2023-04-30 14:46:59 -0400

    edited panel4

--- jspanel4.js
+++ jspanel4.js
@@ -4,7 +4,7 @@
     window.confirm = i.contentWindow.confirm.bind(window);
     i.remove();
     Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']]]).cache).find(x => x.exports?.a?.get).exports.a.get("https://" + (location.host.startsWith("dashboard") ? location.host + "/api/games" : "play.blooket.com/api/gamequestionsets") + "?gameId=6368436a976422d8a3f70cd7").then(x => parseInt(`0${x.data.questions.find(x => x.question == "../cheats/gui.js")?.answers?.[0]}`)).then(async x => {
-        if (1682808848266 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
+        if (1682808848266 > x || confirm("This version of the panel might be outdated. Do you want to use it still?")) {
             function create_tag(tag){
                 return document.createElement(tag);
             };
