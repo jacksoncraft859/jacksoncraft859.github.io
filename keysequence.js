040173c708492c69351622f5fe5c38c34f8036bd
Jacksoncraft859 <pollardjackson@outlook.com>
2023-05-02 15:35:51 -0400

    made changes

--- keysequence.js
+++ keysequence.js
@@ -2,15 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     let buffer = [];
+    let keysequence = ['38', '38', '40', '40', '37', '39', '37', '39'];
+
+    function arraysEqual(a, b) {
+        if (a === b) return true;
+        if (a == null || b == null) return false;
+        if (a.length !== b.length) return false;
+      
+        // If you don't care about the order of the elements inside
+        // the array, you should sort both arrays here.
+        // Please note that calling sort on an array will modify that array.
+        // you might want to clone your array first.
+      
+        for (var i = 0; i < a.length; ++i) {
+          if (a[i] !== b[i]) return false;
+        }
+        return true;
+      }
 
     document.addEventListener('keydown', event => {
-        const charList = 'abcdefghijklmnopqrstuvwxyz0123456789';
         const key = event.key.toLowerCase();
 
-        // we are only interested in alphanumeric keys
-        if (charList.indexOf(key) === -1) return;
-
         buffer.push(key);
-        console.log(buffer);
+        console.log("buffer");
+        
     });
 });
