16b972e529602aca4060122190a8531fdcab7a29
Jacksoncraft859 <pollardjackson@outlook.com>
2023-05-02 15:40:04 -0400

    made changes

--- keysequence.js
+++ keysequence.js
@@ -1,30 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+let buffer = [];
+let keysequence = ['38', '38', '40', '40', '37', '39', '37', '39'];
 
-    let buffer = [];
-    let keysequence = ['38', '38', '40', '40', '37', '39', '37', '39'];
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+    
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+document.addEventListener('keydown', event => {
+    const key = event.key.toLowerCase();
 
-    function arraysEqual(a, b) {
-        if (a === b) return true;
-        if (a == null || b == null) return false;
-        if (a.length !== b.length) return false;
-      
-        // If you don't care about the order of the elements inside
-        // the array, you should sort both arrays here.
-        // Please note that calling sort on an array will modify that array.
-        // you might want to clone your array first.
-      
-        for (var i = 0; i < a.length; ++i) {
-          if (a[i] !== b[i]) return false;
-        }
-        return true;
-      }
-
-    document.addEventListener('keydown', event => {
-        const key = event.key.toLowerCase();
-
-        buffer.push(key);
-        console.log("buffer");
+    buffer.push(key);
+    console.log(buffer);
         
     });
-});
