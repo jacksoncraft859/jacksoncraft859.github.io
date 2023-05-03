b65c7545f1b81ebafa6eec551eedbb341d41c333
Jacksoncraft859 <pollardjackson@outlook.com>
2023-05-03 18:51:54 -0400

    done

--- jspanel4.js
+++ jspanel4.js
@@ -409,48 +409,6 @@ function secret_panel(){
   secretpanel.style.top = "0px";
   secretpanel.style["background-color"] = "black";
   secretpanel.style["color"] = "white";
-  
-  dragElement(document.getElementById("SInjectorPanel"));
-  function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "bar")) {
-      // if present, the header is where you move the DIV from:
-      document.getElementById(elmnt.id + "bar").onmousedown = dragMouseDown;
-    } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-      elmnt.onmousedown = dragMouseDown;
-    };
-
-    function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-    };
-
-    function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-    };
-  };
   secret_subpanel = create_tag("div");
   secret_subpanel.id = "secretsubpanel";
 
@@ -551,6 +509,48 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
+};
+
+sdragElement(document.getElementById("SInjectorPanel"));
+function sdragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "bar")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "bar").onmousedown = dragMouseDown;
+  } else {
+  // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  };
+
+  function dragMouseDown(e) {
+  e = e || window.event;
+  e.preventDefault();
+  // get the mouse cursor position at startup:
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+  document.onmouseup = closeDragElement;
+  // call a function whenever the cursor moves:
+  document.onmousemove = elementDrag;
+  };
+
+  function elementDrag(e) {
+  e = e || window.event;
+  e.preventDefault();
+  // calculate the new cursor position:
+  pos1 = pos3 - e.clientX;
+  pos2 = pos4 - e.clientY;
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+    // set the element's new position:
+  elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+  elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+  document.onmouseup = null;
+  document.onmousemove = null;
+  };
 };
 
 //==================
